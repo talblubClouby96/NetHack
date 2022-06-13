talblubClouby96/NetHack

commit 7160508deee81aef80bca9b9abab10132672cb51

simplify streamlined act_on_act()
More PR #777:  there's no need for there_cmd_menu() to pass absolute
<x,y> instead of <dx,dy> for a couple of actions.  Those actions can
reconstruct <x,y> by adding <dx,dy> to <u.ux,u.uy>.

src/cmd.c
@@ -1,4 +1,4 @@
-/* NetHack 3.7	cmd.c	$NHDT-Date: 1655120485 2022/06/13 11:41:25 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.573 $ */
+/* NetHack 3.7	cmd.c	$NHDT-Date: 1655145035 2022/06/13 18:30:35 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.574 $ */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /*-Copyright (c) Robert Patrick Rankin, 2013. */
 /* NetHack may be freely redistributed.  See license for details. */
@@ -4927,15 +4927,15 @@ there_cmd_menu_common(
 static void
 act_on_act(
     int act,        /* action */
-    int dx, int dy) /* delta to adjacent spot (except for couple of cases) */
+    int dx, int dy) /* delta to adjacent spot (farther for couple of cases) */
 {
     struct obj *otmp;
     int dir;
 
     switch (act) {
     case MCMD_TRAVEL:
-        iflags.travelcc.x = u.tx = dx; /* caller passed x via dx */
-        iflags.travelcc.y = u.ty = dy; /* and y via dy           */
+        iflags.travelcc.x = u.tx = u.ux + dx;
+        iflags.travelcc.y = u.ty = u.uy + dy;
         cmdq_add_ec(dotravel_target);
         break;
     case MCMD_THROW_OBJ:
@@ -5065,8 +5065,8 @@ act_on_act(
         cmdq_add_ec(dolook);
         break;
     case MCMD_LOOK_AT:
-        g.clicklook_cc.x = dx; /* caller passed x via dx */
-        g.clicklook_cc.y = dy; /* and y via dy           */
+        g.clicklook_cc.x = u.ux + dx;
+        g.clicklook_cc.y = u.uy + dy;
         cmdq_add_ec(doclicklook);
         break;
     case MCMD_UNTRAP_HERE:
@@ -5084,8 +5084,6 @@ act_on_act(
         break;
     }
 }
-
-#define act_needs_xy(act) ((act) == MCMD_TRAVEL || (act) == MCMD_LOOK_AT)
 
 /* offer choice of actions to perform at adjacent location <x,y> */
 static char
@@ -5124,8 +5122,7 @@ there_cmd_menu(int x, int y, int mod)
         ch = '\0';
     } else if (K == 1 && act != MCMD_NOTHING) {
         destroy_nhwindow(win);
-        if (act_needs_xy(act))
-            dx = x, dy = y;
+
         act_on_act(act, dx, dy);
         return '\0';
     } else {
@@ -5138,16 +5135,11 @@ there_cmd_menu(int x, int y, int mod)
         act = picks->item.a_int;
         free((genericptr_t) picks);
 
-        /* most actions want <dx,dy> but a few want absolute <x,y> */
-        if (act_needs_xy(act))
-            dx = x, dy = y;
         act_on_act(act, dx, dy);
         return '\0';
     }
     return ch;
 }
-
-#undef act_needs_xy
 
 static char
 here_cmd_menu(void)

commit 68379c9487a67734546a9edda23cca58148922bf

fix 'simplify streamlined act_on_act()'
Still more PR #777.  Commit 7160508dee broke #therecmdmenu travel,
throw, and far-look.  It was restricting dx and dy unnecessarily
and that resulted in not specifying the correct location when the
destination was farther than one step away.

Testing those properly requires a mouse.  I've implemented a way
to simulate a left or right click at getdir()'s prompt (only useful
for #therecmdmenu).  That will be committed separately.

src/cmd.c
@@ -1,4 +1,4 @@
-/* NetHack 3.7	cmd.c	$NHDT-Date: 1655145035 2022/06/13 18:30:35 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.574 $ */
+/* NetHack 3.7	cmd.c	$NHDT-Date: 1655156619 2022/06/13 21:43:39 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.575 $ */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /*-Copyright (c) Robert Patrick Rankin, 2013. */
 /* NetHack may be freely redistributed.  See license for details. */
@@ -5093,7 +5093,8 @@ there_cmd_menu(int x, int y, int mod)
     char ch = '\0';
     int npick = 0, K = 0;
     menu_item *picks = (menu_item *) 0;
-    int dx = sgn(x - u.ux), dy = sgn(y - u.uy);
+    /*int dx = sgn(x - u.ux), dy = sgn(y - u.uy);*/
+    int dx = x - u.ux, dy = y - u.uy;
     int act = MCMD_NOTHING;
 
     win = create_nhwindow(NHW_MENU);